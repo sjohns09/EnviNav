4cb8ba6970b12b1e261fd18c8c61d421fecf4902
Samantha Johnson <sjohns09@terpmail.umd.edu>
2017-12-10 00:07:53 -0500

    #10 - Creation of Framework for global path planner, which is a plugin adhering to the nav_core BaseGlobalPlanner

--- include/RRTPlanner.h
+++ include/RRTPlanner.h
@@ -4,14 +4,34 @@
  *  Created on: Dec 9, 2017
  *      Author: sammie
  */
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
 
 #ifndef ENVI_NAV_SRC_RRTPLANNER_H_
 #define ENVI_NAV_SRC_RRTPLANNER_H_
 
-class RRTPlanner {
+class RRTPlanner : public nav_core::BaseGlobalPlanner {
  public:
   RRTPlanner();
+  RRTPlanner(std::string name, costmap_2d::Costmap2DROS* costmapRos);
+  void initialize (std::string name, costmap_2d::Costmap2DROS* costmapRos);
+  bool makePlan(const geometry_msgs::PoseStamped& start,
+                const geometry_msgs::PoseStamped& goal,
+                std::vector<geometry_msgs::PoseStamped>& plan);
   virtual ~RRTPlanner();
+
+ private:
+  bool is_robot_safe();
+  costmap_2d::Costmap2DROS* _costmapROS;
+  costmap_2d::Costmap2D* _costmap;
+  base_local_planner::WorldModel* _localModel;
+
 };
 
 #endif /* ENVI_NAV_SRC_RRTPLANNER_H_ */
--- src/RRTPlanner.cpp
+++ src/RRTPlanner.cpp
@@ -4,15 +4,57 @@
  *  Created on: Dec 9, 2017
  *      Author: sammie
  */
-
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
 #include "RRTPlanner.h"
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(RRTPlanner, nav_core::BaseGlobalPlanner)
 
 RRTPlanner::RRTPlanner() {
-  // TODO Auto-generated constructor stub
+  ROS_ERROR("Need to call constructor with name and costmap");
+  //_costmap(NULL);
+  //_localModel = NULL;
+}
+
+RRTPlanner::RRTPlanner(std::string name, costmap_2d::Costmap2DROS* costmapRos) {
+  initialize(name, costmapRos);
+}
+
+void RRTPlanner::initialize(std::string name,
+                            costmap_2d::Costmap2DROS* costmapRos) {
+  _costmapROS = costmapRos;
+  _costmap = costmapRos->getCostmap();
+
+  //_localModel = new base_local_planner::CostmapModel(*_costmap);
+
+  ROS_INFO("RRT Planner initialized!!");
+
+}
+
+bool RRTPlanner::is_robot_safe() {
+  std::vector<geometry_msgs::Point> roboBasePrint = _costmapROS->getRobotFootprint();
+
+  return false;
+}
+
+bool RRTPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                          const geometry_msgs::PoseStamped& goal,
+                          std::vector<geometry_msgs::PoseStamped>& plan) {
+
+  ROS_INFO("Making Plan!");
+
+  return true;
 
 }
 
 RRTPlanner::~RRTPlanner() {
-  // TODO Auto-generated destructor stub
+
 }
 
